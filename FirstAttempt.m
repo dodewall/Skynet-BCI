6bb802645deee6f47714df4ccc54ee2267756f05
dodewall <916e0a0c@opayq.com>
2015-04-17 14:29:01 -0400

    Update FirstAttempt.m
    Added linear regression (weight generation)

--- FirstAttempt.m
+++ FirstAttempt.m
@@ -17,12 +17,13 @@ ZeroCrossMeanFn = @ (x) sum( (((x(1:end-1) - mean(x)) > 0) &...
     ((x(2:end) - mean(x)) < 0)) ); %Zero-Crossings Around Mean
 
 AvgFn = @ (x) mean(x);
+% FIRING RATES?
 %featureValues = MovingWinFeats(x, fs, winLen, winDisp, featFn)
 %% PREPROCESSING
 
 % filtering, etc
 
-%% SUBJECT 1
+%% FEATURE EXTRACTION
 
 nChannels = min(size(suj1_train));
 wL = 0.1; %in seconds
@@ -30,8 +31,8 @@ wD = 0.05;
 nWindows = NumWins(length(suj1_train), fs, wL, wD);
 
 nfft = 1024;
-s = spectrogram(suj1_train(:,1),wL*fs,wD*fs,nfft,fs); %rows = frequencies, columns = windows
-fBins = (0:fs/nfft:fs/2)'; %spectrogram frequencies by index
+s = spectrogram(suj1_train(:,1),wL*fs,wD*fs,nfft,fs);
+fBins = (0:fs/nfft:fs/2)'; %frequencies by index
 
 avgTDVoltage = zeros(nWindows, nChannels);
 avgAmp8_12 = zeros(nWindows, nChannels);
@@ -50,17 +51,46 @@ for i = 1:nChannels
     avgAmp159_175(:,i) = mean(abs(s(fBins > 159 & fBins < 175,:)));
 end
 
-% Assemble complete feature matrix
+% Assemble all features
 featureMatrix = [avgTDVoltage avgAmp8_12 avgAmp18_24 avgAmp75_115...
     avgAmp125_159 avgAmp159_175];
-% Scale training features
-[featureMatrix, scaleStd, scaleMean] = ScaleFeatures(featureMatrix);
-
 
 % Downsample dataglove matrix
+% Remove ring finger?
 sep = 50; % 50 ms / 20Hz
 nFingers = min(size(suj1_glove));
 ds_glove = zeros(length(suj1_glove)/sep, nFingers);
 for i = 1:nFingers
     ds_glove(:,i) = decimate(suj1_glove(:,i), sep);
 end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% LINEAR REGRESSION
+
+nBins = 3; %previous time bins to consider
+M_pos = length(ds_glove); %number of position time bins
+M_eeg = length(featureMatrix); %number of EEG time bins
+nPads = (M_pos + nBins - 1) -  M_eeg; %number of EEG bins to pad
+
+%Randomly select EEG time bins to pad feature matrix
+if nPads > 0   
+    pad_indices = randi(M_eeg,[1, nPads]);
+    padded_features = [featureMatrix; featureMatrix(pad_indices,:)]; 
+end
+
+% Assemble linear regression feature matrix
+v = min(size(padded_features));
+features_LinReg = zeros(M_pos,v*nBins);
+for i = 1:M_pos
+    features_LinReg(i,:) = reshape(padded_features(i:i+nBins-1,:),[1 nBins*v]);
+end
+% Add bias (ones) feature, scale features
+[features_LinReg, sc_std, sc_mean] = ScaleFeatures(features_LinReg);
+
+
+% Generate linear regression weights
+weights_1 = mldivide((R'*R), (R'*ds_glove(:,1)));
+weights_2 = mldivide((R'*R), (R'*ds_glove(:,2)));
+weights_3 = mldivide((R'*R), (R'*ds_glove(:,3)));
+weights_4 = mldivide((R'*R), (R'*ds_glove(:,4)));
+weights_5 = mldivide((R'*R), (R'*ds_glove(:,5)));
