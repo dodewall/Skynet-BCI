11276b0fb143ab8d43095cb0a5e395bad60abf7a
dodewall <916e0a0c@opayq.com>
2015-04-18 22:12:55 -0400

    Update mainScript.m

--- mainScript.m
+++ mainScript.m
@@ -8,34 +8,56 @@ loadAnonFunctions
 % FIRING RATES?
 %% PREPROCESSING
 % filtering, etc
+split = 210000;
+
+trainset = suj1_train(1:split,:);
+trainglove = suj1_glove(1:split,:);
+testset = suj1_train(split+1:end,:);
+testglove = suj1_glove(split+1:end,:);
 
 %% MAIN CODE
-n = length(suj3_test);
 fs = 1000; %sampling frequency
 
 % Create R matrix from dataset
-rawR = CreateRMatrix(suj3_test, 3);
+rawR = CreateRMatrix(trainset, 3);
 
 % Add bias (ones) feature, scale features
-%[R, sc_std, sc_mean] = ScaleFeatures(rawR); %Use this line for training
-R = ScaleFeatures(rawR, sc_std, sc_mean); %Use this line for testing
+[R, sc_std, sc_mean] = ScaleFeatures(rawR); %Use this line for training
 
 % Downsample dataglove matrix
-% Remove ring finger?
-ds_glove = Downsample(suj3_glove, 50); % 50 ms / 20Hz
+% Still need to remove 4th finger
+ds_glove = Downsample(trainglove, 50); % 50 ms / 20Hz
 ds_glove = ds_glove(1:end-1,:);
+
+ds_glovetest = Downsample(testglove, 50);
+ds_glovetest = ds_glovetest(1:end-1,:);
 
 % Generate weights
 weights = LinearRegression(R, ds_glove);
+%Lasso Weights have to be done 1 finger at a time
+%[lasso_weights, fitInfo] = lasso(R, ds_glove(:,1)); 
+
+% Generate R matrix for test set
+rawRtest = CreateRMatrix(testset, 3);
+Rtest = ScaleFeatures(rawRtest, sc_std, sc_mean); %Use this line for testing
 
 % Make predictions
-predictions_linreg_ds = R * weights;
+ds_predictions = Rtest * weights;
+
+%lasso_ds_predictions = zeros(length(R_test), 100);
+%lasso_accuracies = zeros(100,1);
+%for i = 1:length(lasso_accuracies)
+%    lasso_ds_predictions(:,i) = R_test*lasso_weights(:,i) + fitInfo.Intercept(i);
+%    lasso_accuracies(i) = corr(lasso_ds_predictions(:,i),ds_testglove(:,1));
+%end
+
 
 % Interpolate predictions
-predictions_linreg = zeros(n, min(size(ds_glove)));
+predictions = zeros(length(testset), min(size(testglove)));
 for i = 1:5
-    predictions_linreg(:,i) = Upsample(predictions_linreg_ds(:,i), 20, 1000, 310);
+    predictions(:,i) = Upsample(ds_predictions(:,i), 20, 1000, length(testglove)/fs);
 end
 
 % Get prediction accuracy
-accuracy = mean(diag(corr(suj3_glove, predictions_linreg)))
+accuracy = mean(diag(corr(testglove, predictions)))
+ds_accuracy = mean(diag(corr(ds_glovetest, ds_predictions)))
