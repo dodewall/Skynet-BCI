7d229b91e0d6581150ce7508a41e0c634babfefa
dodewall <916e0a0c@opayq.com>
2015-04-21 22:48:07 -0400

    Update ScaleFeatures.m

--- ScaleFeatures.m
+++ ScaleFeatures.m
@@ -5,18 +5,20 @@ function [scaled_features, sc_std, sc_mean] = ScaleFeatures(features, varargin)
 
 % Add bias feature
 sz = size(features);
-features = [ones(sz(1), 1) features];
+nRows = sz(1);
+features = [ones(nRows, 1) features];
 
 switch nargin  
     case 3
         sc_std = varargin{1};
         sc_mean = varargin{2};
     otherwise
-        sc_std = std(features(:));
-        sc_mean = mean(features(:));
+        sc_std = std(features,[],1);
+        sc_mean = mean(features,1);
         
 end
 
-scaled_features = (features - sc_mean) ./ sc_std;
+scaled_features = (features - repmat(sc_mean, nRows, 1)) ./ ...
+    repmat(sc_std, nRows, 1);
     
 end
